172869632cf40e2e7e7aceb18139c307e226b35f
Ashley <ashleymsutton@outlook.com>
2016-09-14 11:41:06 -0400

    added blog/post_list template

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# So your views is like your controller in the sense that it looks to the views.py folder after a request for post_list url is made...
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
