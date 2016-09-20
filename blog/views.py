5f38e24876307dc42c9438e1590103d715a28ef5
Ashley <ashleymsutton@outlook.com>
2016-09-19 22:16:05 -0400

    imported the 'Post' model into views.py so that it may be be used in the template'

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+# Here we are importing the models we want to include in out template into our views.py
+from .models import Post
 # Create your views here.
 
 # So your views is like your controller in the sense that it looks to the views.py folder after a request for post_list url is made...
