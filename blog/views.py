dbe7df51d0eee55a346c8cb4c84714c99e0ed2e0
Ashley <ashleymsutton@outlook.com>
2016-09-19 22:23:12 -0400

    import timezone because it is used in the QuerySet for 'posts'

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+#importing timezone because we are using it in our 'posts' QuerySet.
+from django.utils import timezone
 # Here we are importing the models we want to include in out template into our views.py
 from .models import Post
+
+
 # Create your views here.
 
 # So your views is like your controller in the sense that it looks to the views.py folder after a request for post_list url is made...
