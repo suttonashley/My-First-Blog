8a5f4ee66fc625ce87dc3f7018f342cd4559232d
Ashley <ashleymsutton@outlook.com>
2016-09-19 22:20:58 -0400

    created a variable for the QuerySet under post_list and labled it 'posts'

--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,5 @@ from .models import Post
 
 # So your views is like your controller in the sense that it looks to the views.py folder after a request for post_list url is made...
 def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {})
