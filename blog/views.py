3b73a6ef0a24579cff0d3328a4f425834dc26d06
Ashley <ashleymsutton@outlook.com>
2016-09-20 20:45:19 -0400

    completed post_details view, created a template for the post_details and added logic to only display the queried post

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,3 @@
-from django.shortcuts import render
-
 from django.shortcuts import render, get_object_or_404
 #importing timezone because we are using it in our 'posts' QuerySet.
 from django.utils import timezone
@@ -15,3 +13,5 @@ def post_list(request):
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_details(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_details.html', {'post' : post})
