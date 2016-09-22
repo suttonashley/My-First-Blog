b5d021f5c859cc45c08d7ac090f5e0b08fa94fc2
Ashley <ashleymsutton@outlook.com>
2016-09-22 11:04:08 -0400

    syntax fix in post_new redirect

--- blog/views.py
+++ blog/views.py
@@ -22,8 +22,6 @@ def post_details(request, pk):
     return render(request, 'blog/post_details.html', {'post' : post})
 
 def post_new(request):
-    form = PostForm()
-    return render(request.POST, 'blog/post_edit.html', {'form': form})
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
@@ -31,5 +29,7 @@ def post_new(request):
             post.author = request.user
             post.published_date = timezone.now()
             post.save()
+            return redirect('post_details', pk=post.pk)
     else:
         form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
