882951ef503d8d19004579bafb4a5ae211b5242f
Ashley <ashleymsutton@outlook.com>
2016-09-22 10:18:32 -0400

    added form by making changes to the views, the url, and the template for the form

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^post/(?P<pk>\d+)/$', views.post_details, name='post_details')
+    url(r'^post/(?P<pk>\d+)/$', views.post_details, name='post_details'),
+    url(r'^post/new/$', views.post_new, new='post_new'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,14 +4,20 @@ from django.utils import timezone
 # Here we are importing the models we want to include in out template into our views.py
 from .models import Post
 
+from .views import PostForm
+
 
 # Create your views here.
 
 # So your views is like your controller in the sense that it looks to the views.py folder after a request for post_list url is made...
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    return render(request, 'blog/post_list.html', {'posts': posts} )
 
 def post_details(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_details.html', {'post' : post})
+
+def post_view(request):
+    form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
